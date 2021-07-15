5a255964f3160577600c6b1d650184865a2d726a
Ptrak <pat.cowan@outlook.com>
2021-07-15 02:44:02 +0000

    Solved problem 9

---
+++ problem9/solution.py
@@ -0,0 +1,208 @@
+
+def add(program, pc, mode1, mode2, mode3):
+    if mode1 == 0:
+        lhs_val = int(program[int(program[pc+1])])
+    elif mode1 == 1:
+        lhs_val = int(program[pc+1])
+    elif mode1 == 2:
+        lhs_val = int(program[int(program[pc+1]) + offset])
+
+    if mode2 == 0:
+        rhs_val = int(program[int(program[pc+2])])
+    elif mode2 == 1:
+        rhs_val = int(program[pc+2])
+    elif mode2 == 2:
+        rhs_val = int(program[int(program[pc+2]) + offset])
+
+    result = lhs_val + rhs_val
+    #print("{} + {} = {}".format(lhs_val, rhs_val, result))
+    if mode3 == 0 or mode3 == 1:
+        program[int(program[pc+3])] = str(result)
+    elif mode3 == 2:
+        program[int(program[pc+3]) + offset] = str(result)
+
+    return pc + 4
+
+def multiply(program, pc, mode1, mode2, mode3):
+    if mode1 == 0:
+        lhs_val = int(program[int(program[pc+1])])
+    elif mode1 == 1:
+        lhs_val = int(program[pc+1])
+    elif mode1 == 2:
+        lhs_val = int(program[int(program[pc+1]) + offset])
+
+    if mode2 == 0:
+        rhs_val = int(program[int(program[pc+2])])
+    elif mode2 == 1:
+        rhs_val = int(program[pc+2])
+    elif mode2 == 2:
+        rhs_val = int(program[int(program[pc+2]) + offset])
+
+    result = lhs_val * rhs_val
+    if mode3 == 0 or mode3 == 1:
+        program[int(program[pc+3])] = str(result)
+    elif mode3 == 2:
+        program[int(program[pc+3]) + offset] = str(result)
+    return pc + 4
+
+def read(program, pc, mode1):
+    if mode1 == 0:
+        location = int(program[pc+1])
+    elif mode1 == 1:
+        location = pc + 1
+        print("Value: {}".format(program[pc+1]))
+        return pc + 2
+    elif mode1 == 2:
+        location = int(program[pc+1]) + offset
+        #print("location {} offset: {}".format(location, offset))
+
+    print("Value: {}".format(program[location]))
+    return pc + 2
+
+def jit(program, pc, mode1, mode2):
+    if mode1 == 0:
+        test_val = int(program[int(program[pc+1])])
+    elif mode1 == 1:
+        test_val = int(program[pc + 1])
+    elif mode1 == 2:
+        test_val = int(program[int(program[pc+1]) + offset])
+
+    if mode2 == 0:
+        jump_val = int(program[int(program[pc+2])])
+    elif mode2 == 1:
+        jump_val = int(program[pc+2])
+    elif mode2 == 2:
+        jump_val = int(program[int(program[pc+2]) + offset])
+
+    if test_val != 0:
+        return jump_val
+    return pc + 3
+
+def jnt(program, pc, mode1, mode2):
+    if mode1 == 0:
+        test_val = int(program[int(program[pc+1])])
+    elif mode1 == 1:
+        test_val = int(program[pc + 1])
+    elif mode1 == 2:
+        test_val = int(program[int(program[pc+1]) + offset])
+
+    if mode2 == 0:
+        jump_val = int(program[int(program[pc+2])])
+    elif mode2 == 1:
+        jump_val = int(program[pc+2])
+    elif mode2 == 2:
+        jump_val = int(program[int(program[pc+2]) + offset])
+
+    if test_val == 0:
+        return jump_val
+    return pc + 3
+
+def lt(program, pc, mode1, mode2, mode3):
+    if mode1 == 0:
+        lhs_val = int(program[int(program[pc+1])])
+    elif mode1 == 1:
+        lhs_val = int(program[pc+1])
+    elif mode1 == 2:
+        lhs_val = int(program[int(program[pc+1]) + offset])
+
+    if mode2 == 0:
+        rhs_val = int(program[int(program[pc+2])])
+    elif mode2 == 1:
+        rhs_val = int(program[pc+2])
+    elif mode2 == 2:
+        rhs_val = int(program[int(program[pc+2]) + offset])
+
+    result = int(lhs_val < rhs_val)
+    if mode3 == 0 or mode3 == 1:
+        program[int(program[pc+3])] = str(result)
+    elif mode3 == 2:
+        program[int(program[pc+3]) + offset] = str(result)
+    return pc + 4
+
+def eq(program, pc, mode1, mode2, mode3):
+    if mode1 == 0:
+        lhs_val = int(program[int(program[pc+1])])
+    elif mode1 == 1:
+        lhs_val = int(program[pc+1])
+    elif mode1 == 2:
+        lhs_val = int(program[int(program[pc+1]) + offset])
+
+    if mode2 == 0:
+        rhs_val = int(program[int(program[pc+2])])
+    elif mode2 == 1:
+        rhs_val = int(program[pc+2])
+    elif mode2 == 2:
+        rhs_val = int(program[int(program[pc+2]) + offset])
+
+    result = int(lhs_val == rhs_val)
+    if mode3 == 0 or mode3 == 1:
+        program[int(program[pc+3])] = str(result)
+    elif mode3 == 2:
+        program[int(program[pc+3]) + offset] = str(result)
+    return pc + 4
+
+def rbo(program, pc, mode1):
+    global offset
+    if mode1 == 0:
+        offset += int(program[int(program[pc+1])])
+    elif mode1 == 1:
+        offset += int(program[pc+1])
+    elif mode1 == 2:
+        offset += int(program[int(program[pc+1]) + offset])
+    return pc + 2
+
+global offset
+offset = 0
+
+global memory
+memory = [0 for _ in range(100000)]
+
+pc = 0
+#f = open('test_input.txt', 'r')
+f = open('input.txt', 'r')
+line = f.readline().rstrip()
+program = line.split(',')
+program.extend(memory)
+
+while(1):
+    instruction = program[pc]
+    while(len(instruction) < 5) :
+        instruction = "0" + instruction
+    mode3  = int(instruction[0])
+    mode2  = int(instruction[1])
+    mode1  = int(instruction[2])
+    opcode = int(instruction[3:])
+    # add
+    if opcode == 1:
+        pc = add(program, pc, mode1, mode2, mode3)
+    elif opcode == 2:
+        pc = multiply(program, pc, mode1, mode2, mode3)
+    elif opcode == 3:
+        if mode1 == 0 or mode1 == 1:
+            location = int(program[pc+1])
+        elif mode1 == 2:
+            location = int(int(program[pc+1]) + offset)
+        val = input("Enter your value: ")
+        program[location] = val
+
+
+        #print("  Saved to location: {} Value: {}".format(location, val))
+        pc += 2
+    elif opcode == 4:
+        pc = read(program, pc, mode1)
+    elif opcode == 5:
+        pc = jit(program, pc, mode1, mode2)
+    elif opcode == 6:
+        pc = jnt(program, pc, mode1, mode2)
+    elif opcode == 7:
+        pc = lt(program, pc, mode1, mode2, mode3)
+    elif opcode == 8:
+        pc = eq(program, pc, mode1, mode2, mode3)
+    elif opcode == 9:
+        pc = rbo(program, pc, mode1)
+    elif opcode == 99:
+        print ('done!')
+        break
+    else:
+        print("invalid opcode!!!", opcode)
+        break
---
+++ problem9/test.py
@@ -0,0 +1,8 @@
+def modifyGlobal(value):
+    global globe
+    globe = value
+
+modifyGlobal(10)
+print(globe)
+modifyGlobal(11)
+print(globe)
